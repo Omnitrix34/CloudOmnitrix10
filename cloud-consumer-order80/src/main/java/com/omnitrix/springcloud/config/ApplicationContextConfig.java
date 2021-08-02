7fa081d777f0fee7c7d82a52077ff59c71667cd8
Omnitrix10 <2741626189@qq.com>
2021-08-02 08:59:25 +0800

    eureka集群+生产者集群

--- cloud-consumer-order80/src/main/java/com/omnitrix/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/omnitrix/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.omnitrix.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ public class ApplicationContextConfig {
     //往容器中添加一个RestTemplate
     //RestTemplate提供了多种便捷访问远程http访问的方法
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
